e879d60c0e76d0001e0735ef9de4135eabf5693e
Wesley Adriann <wesleyadriann@gmail.com>
2025-12-08 10:51:22 -0300

    adds layer 4
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- keyboards/sofle/keymaps/wesleyadriann/keymap.c
+++ keyboards/sofle/keymaps/wesleyadriann/keymap.c
@@ -6,6 +6,7 @@
 
 enum custom_keycodes {
     LAYER_TG_LT = SAFE_RANGE, // tecla customizada
+    LAYER_TG_LT_2
 };
 
 static uint16_t layer_timer;
@@ -28,29 +29,44 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false; // não envia keycode padrão
+        case LAYER_TG_LT_2:
+            if (record->event.pressed) {
+                layer_timer = timer_read();
+                layer_on(4);  // ativa layer 4 enquanto segura
+            } else {
+                layer_off(4);  // soltar → desativa layer 4
+                if (timer_elapsed(layer_timer) < 200) {
+                    // toque rápido → alterna layer 2
+                    layer_invert(2);
+                } else {
+                    // segurou → volta pra layer 0
+                    layer_move(0);
+                }
+            }
+            return false; // não envia keycode padrão
     }
     return true;
 }
 
 // Keymaps
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//        ┌─────────┬──────┬──────┬──────┬─────────────┬─────┐                 ┌─────┬───────┬──────┬──────┬──────┬──────┐
-//        │ QK_GESC │  1   │  2   │  3   │      4      │  5  │                 │  6  │   7   │  8   │  9   │  0   │  -   │
-//        ├─────────┼──────┼──────┼──────┼─────────────┼─────┤                 ├─────┼───────┼──────┼──────┼──────┼──────┤
-//        │   tab   │  q   │  w   │  e   │      r      │  t  │                 │  y  │   u   │  i   │  o   │  p   │  =   │
-//        ├─────────┼──────┼──────┼──────┼─────────────┼─────┤                 ├─────┼───────┼──────┼──────┼──────┼──────┤
-//        │  lsft   │  a   │  s   │  d   │      f      │  g  │                 │  h  │   j   │  k   │  l   │  ;   │  '   │
-//        ├─────────┼──────┼──────┼──────┼─────────────┼─────┼──────┐   ┌──────┼─────┼───────┼──────┼──────┼──────┼──────┤
-//        │  lctl   │  z   │  x   │  c   │      v      │  b  │ mute │   │ mply │  n  │   m   │  ,   │  .   │  /   │ rsft │
-//        └─────────┼──────┼──────┼──────┼─────────────┼─────┼──────┘   └──────┼─────┼───────┼──────┼──────┼──────┼──────┘
-//                  │ lgui │ lalt │ caps │ LAYER_TG_LT │ spc │                 │ ent │ TG(2) │ bspc │ ralt │ rgui │
-//                  └──────┴──────┴──────┴─────────────┴─────┘                 └─────┴───────┴──────┴──────┴──────┘
+//        ┌─────────┬──────┬──────┬──────┬─────────────┬─────┐                 ┌─────┬───────────────┬──────┬──────┬──────┬──────┐
+//        │ QK_GESC │  1   │  2   │  3   │      4      │  5  │                 │  6  │       7       │  8   │  9   │  0   │  -   │
+//        ├─────────┼──────┼──────┼──────┼─────────────┼─────┤                 ├─────┼───────────────┼──────┼──────┼──────┼──────┤
+//        │   tab   │  q   │  w   │  e   │      r      │  t  │                 │  y  │       u       │  i   │  o   │  p   │  =   │
+//        ├─────────┼──────┼──────┼──────┼─────────────┼─────┤                 ├─────┼───────────────┼──────┼──────┼──────┼──────┤
+//        │  lsft   │  a   │  s   │  d   │      f      │  g  │                 │  h  │       j       │  k   │  l   │  ;   │  '   │
+//        ├─────────┼──────┼──────┼──────┼─────────────┼─────┼──────┐   ┌──────┼─────┼───────────────┼──────┼──────┼──────┼──────┤
+//        │  lctl   │  z   │  x   │  c   │      v      │  b  │ mute │   │ mply │  n  │       m       │  ,   │  .   │  /   │ rsft │
+//        └─────────┼──────┼──────┼──────┼─────────────┼─────┼──────┘   └──────┼─────┼───────────────┼──────┼──────┼──────┼──────┘
+//                  │ lgui │ lalt │ caps │ LAYER_TG_LT │ spc │                 │ ent │ LAYER_TG_LT_2 │ bspc │ ralt │ rgui │
+//                  └──────┴──────┴──────┴─────────────┴─────┘                 └─────┴───────────────┴──────┴──────┴──────┘
 [0] = LAYOUT(
-      QK_GESC , KC_1    , KC_2    , KC_3    , KC_4        , KC_5   ,                         KC_6   , KC_7  , KC_8    , KC_9    , KC_0    , KC_MINS,
-      KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R        , KC_T   ,                         KC_Y   , KC_U  , KC_I    , KC_O    , KC_P    , KC_EQL ,
-      KC_LSFT , KC_A    , KC_S    , KC_D    , KC_F        , KC_G   ,                         KC_H   , KC_J  , KC_K    , KC_L    , KC_SCLN , KC_QUOT,
-      KC_LCTL , KC_Z    , KC_X    , KC_C    , KC_V        , KC_B   , KC_MUTE ,     KC_MPLY , KC_N   , KC_M  , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT,
-                KC_LGUI , KC_LALT , KC_CAPS , LAYER_TG_LT , KC_SPC ,                         KC_ENT , TG(2) , KC_BSPC , KC_RALT , KC_RGUI
+      QK_GESC , KC_1    , KC_2    , KC_3    , KC_4        , KC_5   ,                         KC_6   , KC_7          , KC_8    , KC_9    , KC_0    , KC_MINS,
+      KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R        , KC_T   ,                         KC_Y   , KC_U          , KC_I    , KC_O    , KC_P    , KC_EQL ,
+      KC_LSFT , KC_A    , KC_S    , KC_D    , KC_F        , KC_G   ,                         KC_H   , KC_J          , KC_K    , KC_L    , KC_SCLN , KC_QUOT,
+      KC_LCTL , KC_Z    , KC_X    , KC_C    , KC_V        , KC_B   , KC_MUTE ,     KC_MPLY , KC_N   , KC_M          , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT,
+                KC_LGUI , KC_LALT , KC_CAPS , LAYER_TG_LT , KC_SPC ,                         KC_ENT , LAYER_TG_LT_2 , KC_BSPC , KC_RALT , KC_RGUI
 ),
 
 //        ┌─────┬─────┬─────┬─────┬─────┬─────┐               ┌──────┬──────┬─────┬──────┬─────┬─────┐
@@ -108,6 +124,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS   , KC_TRNS   , KC_TRNS ,                         KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_TRNS , KC_TRNS,
       KC_TRNS , KC_TRNS , KC_TRNS , LCS(KC_C) , LCS(KC_V) , KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
                 KC_TRNS , KC_TRNS , KC_TRNS   , KC_TRNS   , KC_TRNS ,                         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
+),
+
+//        ┌─────┬─────┬─────┬─────┬─────┬─────┐               ┌─────┬─────┬─────┬─────┬─────┬─────┐
+//        │     │     │     │     │     │     │               │     │     │     │     │     │     │
+//        ├─────┼─────┼─────┼─────┼─────┼─────┤               ├─────┼─────┼─────┼─────┼─────┼─────┤
+//        │     │     │     │  [  │  ]  │     │               │     │     │     │     │     │     │
+//        ├─────┼─────┼─────┼─────┼─────┼─────┤               ├─────┼─────┼─────┼─────┼─────┼─────┤
+//        │     │     │     │  (  │  )  │     │               │     │     │     │     │     │     │
+//        ├─────┼─────┼─────┼─────┼─────┼─────┼─────┐   ┌─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+//        │     │     │     │  {  │  }  │     │     │   │     │     │     │     │     │     │     │
+//        └─────┼─────┼─────┼─────┼─────┼─────┼─────┘   └─────┼─────┼─────┼─────┼─────┼─────┼─────┘
+//              │     │     │     │     │     │               │     │     │     │     │     │
+//              └─────┴─────┴─────┴─────┴─────┘               └─────┴─────┴─────┴─────┴─────┘
+[4] = LAYOUT(
+      KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
+      KC_TRNS , KC_TRNS , KC_TRNS , KC_LBRC , KC_RBRC , KC_TRNS ,                         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
+      KC_TRNS , KC_TRNS , KC_TRNS , KC_LPRN , KC_RPRN , KC_TRNS ,                         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
+      KC_TRNS , KC_TRNS , KC_TRNS , KC_LCBR , KC_RCBR , KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
+                KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
 )
 };
 
